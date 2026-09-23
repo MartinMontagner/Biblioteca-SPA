50e66733d1c11fc892dc547362ae84ccad22e035
MartinMontagner <martinmontagner97@gmail.com>
2026-09-23 09:18:04 -0300

    first commit

---
+++ js/data.js
@@ -0,0 +1,92 @@
+let libros = [
+  {
+    id: 1,
+    titulo: "Cien años de soledad",
+    autor: "Gabriel García Márquez",
+    genero: "Realismo mágico",
+    año: 1967,
+    disponible: true,
+    favorito: true
+  },
+  {
+    id: 2,
+    titulo: "1984",
+    autor: "George Orwell",
+    genero: "Ciencia ficción",
+    año: 1949,
+    disponible: false,
+    favorito: true
+  },
+  {
+    id: 3,
+    titulo: "El principito",
+    autor: "Antoine de Saint-Exupéry",
+    genero: "Fantasía",
+    año: 1943,
+    disponible: true,
+    favorito: false
+  },
+  {
+    id: 4,
+    titulo: "Rayuela",
+    autor: "Julio Cortázar",
+    genero: "Novela",
+    año: 1963,
+    disponible: true,
+    favorito: true
+  },
+  {
+    id: 5,
+    titulo: "El señor de los anillos",
+    autor: "J.R.R. Tolkien",
+    genero: "Fantasía",
+    año: 1954,
+    disponible: false,
+    favorito: false
+  },
+  {
+    id: 6,
+    titulo: "Crónica de una muerte anunciada",
+    autor: "Gabriel García Márquez",
+    genero: "Novela",
+    año: 1981,
+    disponible: true,
+    favorito: false
+  },
+  {
+    id: 7,
+    titulo: "Fahrenheit 451",
+    autor: "Ray Bradbury",
+    genero: "Ciencia ficción",
+    año: 1953,
+    disponible: true,
+    favorito: true
+  },
+  {
+    id: 8,
+    titulo: "Orgullo y prejuicio",
+    autor: "Jane Austen",
+    genero: "Romance",
+    año: 1813,
+    disponible: false,
+    favorito: false
+  },
+  {
+    id: 9,
+    titulo: "El hobbit",
+    autor: "J.R.R. Tolkien",
+    genero: "Fantasía",
+    año: 1937,
+    disponible: true,
+    favorito: true
+  },
+  {
+    id: 10,
+    titulo: "Don Quijote de la Mancha",
+    autor: "Miguel de Cervantes",
+    genero: "Novela",
+    año: 1605,
+    disponible: true,
+    favorito: false
+  }
+];
